afd8fc523835d6053332302b79f9b963d8270816
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2024-05-14 16:55:29 -0500

    actualizar

---
+++ repaso-clase.js
@@ -0,0 +1,37 @@
+function personaP(nombre,apellido){
+  this.nombre = nombre;
+  this.apellido= apellido;
+}
+personaP.prototype.decirNombre=function(){
+  return ´Mi Nomre es ${this.nombre} ${this.apellido}´;   
+}
+
+function trabajadorP(nombre,apellido,profesion){
+  // llame al metodo constructor padre con (call)
+  personaP.call(this.Nomre,apellido);
+  this.profesion=profesion;
+}
+
+// estancias de la clase
+trabajadorP.prototype = Object.create(personaP.prototype);
+trabajadorP.prototype = constructor = trabajadorP;
+trabajadorP.prototype = decirProfesion = function(){
+  return ´yo trabajo en ${this.profesion}´;
+}
+
+// estancia de la clase
+let persona=new personaP("Diego,Novoa")
+persona = decirNombre();
+
+/////  en ECMAScript 6 tenemos adceso a las clases keyboards 
+
+class personaC{
+  constructor(nombre,apellido){
+    this.nombre = nombre;
+    this.apellido= apellido;
+  }
+  decirNombre(){
+    return ´Mi Nomre es ${this.nombre} ${this.apellido}´;
+ }
+}
+
